799c7f0fb39d0997b9b224a96c36affae4eb8b82
Pinkylab <order66.rgxa2@gmail.com>
2017-08-14 05:32:41 +0900

    Spotifyなうぷれ機能実装

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const {
 } = require('electron');
 
 var itunes = require('playback');
+var applescript = require('applescript');
 
 // ズームを禁止する
 const webFrame = require('electron').webFrame;
@@ -38,10 +39,7 @@ function keydown() {
   if (((event.ctrlKey && !event.metaKey) || (!event.ctrlKey && event.metaKey)) &&
     event.shiftKey &&
     event.keyCode == 77) {
-
-    itunes.currentTrack(function (track) {
-      document.forms["tweetform"].elements["tweettext"].value += '♪ ' + track.name + ' - ' + track.artist;
-    });
+    getNowplaying();
   }
 }
 
@@ -53,4 +51,45 @@ function tweet(str) {
   // メッセージ送信
   console.log('RendererProcess: tweetを BrowserProcess に送信');
   ipcRenderer.send('asynchronous-tweet', str);
+}
+
+
+
+function getNowplaying() {
+  itunes.currentTrack(function (track) {
+    if (track) {
+      // テキストボックスに挿入
+      document.forms["tweetform"].elements["tweettext"].value +=
+        track.name + " - " + track.album + ' by ' + track.artist + '#nowplaying';
+    } else {
+      // iTunesが取れなかったらSpotifyを取ってくる
+      var script = `
+property spotPause : «constant ****kPSp»
+property spotPlay : «constant ****kPSP»
+set ret to ""
+if application "Spotify" is running then
+  tell application "Spotify" to set playerState to player state
+  if playerState is spotPlay then
+    tell application "Spotify"
+      set currentArtist to artist of current track as string
+      set currentTrack to name of current track as string
+      set currentAlbum to album of current track as string
+      set ret to currentTrack & " - " & currentAlbum & " by " & currentArtist & " #nowplaying"
+    end tell
+  end if
+end if
+delay 1
+return ret
+`;
+      applescript.execString(script, function (err, result) {
+        if (err) {
+          // Something went wrong! 
+          console.log('RendererProcess: ' + err);
+        } else {
+          // テキストボックスに挿入
+          document.forms["tweetform"].elements["tweettext"].value += result;
+        }
+      });
+    }
+  });
 }
